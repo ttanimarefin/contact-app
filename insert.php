efe9ccc1cf9da1efe6ea423fddb8c6ffcd7f9de9
ttanimarefin <ttanimarefin@gmail.com>
2021-03-14 02:40:53 +0600

    form validaton

--- insert.php
+++ insert.php
@@ -9,13 +9,19 @@ include('db.php');
      $description=$_POST['description'];
      $date=date('Y-m-d');
 
+     if(!empty($name)&&!empty($address)&&!(email)){
+
+     
+
      $sql = "INSERT INTO clients (name,email,phone,address,description,created)VALUES('$name','$email','$phone','$address','$description','$date')";
      var_dump($sql);
      $result=mysqli_query($db,$sql);
      if($result==true){
         header('location:index.php');
      }
-
+   }else{
+      echo"must be add name,email,address of client".'<a href="index.php">Go Back</a>';
+   }
 
 
 ?>
